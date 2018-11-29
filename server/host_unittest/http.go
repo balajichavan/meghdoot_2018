c21ec4499ad9c6906b1d060aead98957eea5a580
Balaji Chavan -X (bchavan - L Bchavan T TECHNOLOGY SERVICES LIMITED at Cisco) <bchavan@cisco.com>
2018-11-29 15:37:21 +0530

    Maintain switchsate in service and modify only on POST operation http POST /switch?state=ON

--- server/host_unittest/http.go
+++ server/host_unittest/http.go
@@ -12,25 +12,28 @@ var switchBool int = 1
 
 func switchHandler(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	if  switchState == "ON" {
-		switchState = "OFF"
-	} else {
-		switchState = "ON"
-	}
+
 	
 	if ( r.Method == "GET") {
 	
 		fmt.Fprintf(w, "{ \"status\" : \"%s\" }", switchState )
-		log.Printf(
+		
+	}	
+	if ( r.Method == "POST" ) {
+		if  switchState == "ON" {
+			switchState = "OFF"
+		} else {
+			switchState = "ON"
+		}
+		fmt.Fprintf(w, "{ \"status\" : \"%s\" }", switchState )
+	}
+	log.Printf(
             "%s\t%s\t%s\t%s",
             r.Method,
             r.RequestURI,
             time.Since(start),
 			switchState,
         )
-	}	else {
-		log.Printf("Method not supported")
-	}
 }
 
 
@@ -40,27 +43,28 @@ func remotehandler(w http.ResponseWriter, r *http.Request) {
         //NEW CODE
     fmt.Println("URL.String is %s",r.URL.String())
 
-        fmt.Printf("\nparsed key :  %s",k)
-        ip := r.URL.Query().Get("stbip")
-        fmt.Printf("\nstbip :  %s , key : %s ",ip, k)
-        cmd := fmt.Sprintf("cmd2000 %s \"osdiag %s\"",ip, k)
+    fmt.Printf("\nparsed key :  %s",k)
+    ip := r.URL.Query().Get("stbip")
+    fmt.Printf("\nstbip :  %s , key : %s ",ip, k)
+    cmd := fmt.Sprintf("cmd2000 %s \"osdiag %s\"",ip, k)
         //Handle key including + symbol, its dropped by HTTP URL, append it again
         if ( (k == "c") || (k == "v") || (k == "p") || ( k == "d") ) {
                 cmd = fmt.Sprintf("cmd2000 %s \"osdiag %s+\"",ip, k)
         }
         fmt.Printf("\nCommand is  %s : ",cmd)
-    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    w.WriteHeader(http.StatusOK)
-    if err := json.NewEncoder(w).Encode(jsonErr{Code : http.StatusOK , Text : "Success"} ); err != nil {
-                panic(err)
-        }
+    //w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    //w.WriteHeader(http.StatusOK)
+    //if err := json.NewEncoder(w).Encode(jsonErr{Code : http.StatusOK , Text : "Success"} ); err != nil {
+     //           panic(err)
+    //    }
 
-    out, err :=  exec.Command("sh","-c",cmd ).Output()
-    if(err != nil){
-                fmt.Printf("%s",err)
-        }else{
-                fmt.Printf("%s",out);
-        }}
+    //out, err :=  exec.Command("sh","-c",cmd ).Output()
+    //if(err != nil){
+    //            fmt.Printf("%s",err)
+    //    }else{
+    //            fmt.Printf("%s",out);
+    //    }
+	}
 
 func stbinfohandler(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
